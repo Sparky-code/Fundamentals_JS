ba305e7c7f9051a29d86d9d10e370fca26f2abd6
Sparky-code <dkrizwold@berkeley.edu>
2022-01-11 12:01:18 -0800

    Fundamentals js work

---
+++ js_tasks.js
@@ -0,0 +1,17 @@
+
+let admin, Name; // multi declaration using commas
+
+Name = "John";
+
+admin = Name
+
+function alert( admin ); // "John"
+
+let ourPlanet = "Earth"; // how would you name our planet as a let?
+
+let currentUsername = "John" // current visiter to a store site
+
+console.log(23+45)
+
+
+
